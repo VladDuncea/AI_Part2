97fc46c949aeb2a38333401e8225024e89fafb25
VladDuncea <vlad_duncea_31@yahoo.com>
2020-04-25 21:39:49 +0300

    Finished A* alg for Lab4

--- Tema1(Lab4-6)/a_star_complet.py
+++ Tema1(Lab4-6)/a_star_complet.py
@@ -1,6 +1,4 @@
-
-
-""" definirea problemei """
+# definirea problemei
 class Nod:
 	def __init__(self, info, h):
 		self.info = info
@@ -8,6 +6,7 @@ class Nod:
 		
 	def __str__ (self):
 		return "({}, h={})".format(self.info, self.h)
+
 	def __repr__ (self):
 		return f"({self.info}, h={self.h})"
 
@@ -17,7 +16,8 @@ class Arc:
 		self.capat = capat
 		self.varf = varf
 		self.cost = cost
-		
+
+
 class Problema:
 	def __init__(self):
 		self.noduri = [
@@ -44,113 +44,101 @@ class Problema:
 		]
 		self.nod_start = self.noduri[0]		# de tip Nod
 		self.nod_scop = 'f'					# doar info (fara h)
-				
 
-
-		
 	def cauta_nod_nume(self, info):
-		"""Stiind doar informatia "info" a unui nod,
-		trebuie sa returnati fie obiectul de tip Nod care are acea informatie, 
-		fie None, daca nu exista niciun nod cu acea informatie."""
-		pass # TO DO...
-		
+		for nod in self.noduri:
+			if nod.info == info:
+				return nod
+		return None
+# Sfarsit definire problema
 
-		
-""" Sfarsit definire problema """	
+# Clase folosite in algoritmul A*
 
 
-			
-""" Clase folosite in algoritmul A* """
-		
 class NodParcurgere:
 	"""O clasa care cuprinde informatiile asociate unui nod din listele open/closed
 		Cuprinde o referinta catre nodul in sine (din graf)
 		dar are ca proprietati si valorile specifice algoritmului A* (f si g). 
 		Se presupune ca h este proprietate a nodului din graf
-	
 	"""
-
 	problema = None		# atribut al clasei
-
 
 	def __init__(self, nod_graf, parinte=None, g=0, f=None):
 		self.nod_graf = nod_graf	# obiect de tip Nod
-		self.parinte = parinte		# obiect de tip Nod
+		self.parinte = parinte		# obiect de tip NodParcurgere
 		self.g = g					# costul drumului de la radacina pana la nodul curent
 		if f is None :
 			self.f = self.g + self.nod_graf.h
 		else:
 			self.f = f
 
-
 	def drum_arbore(self):
-		"""
-			Functie care calculeaza drumul asociat unui nod din arborele de cautare.
-			Functia merge din parinte in parinte pana ajunge la radacina
-		"""
+		"""Functie care calculeaza drumul asociat unui nod din arborele de cautare.
+			Functia merge din parinte in parinte pana ajunge la radacina"""
 		nod_c = self
 		drum = [nod_c]
-		while nod_c.parinte is not None :
+		while nod_c.parinte is not None:
 			drum = [nod_c.parinte] + drum
 			nod_c = nod_c.parinte
 		return drum
 
-
 	def contine_in_drum(self, nod):
-		"""
-			Functie care verifica daca nodul "nod" se afla in drumul dintre radacina si nodul curent (self).
+		"""Functie care verifica daca nodul "nod" se afla in drumul dintre radacina si nodul curent (self).
 			Verificarea se face mergand din parinte in parinte pana la radacina
 			Se compara doar informatiile nodurilor (proprietatea info)
 			Returnati True sau False.
 
 			"nod" este obiect de tip Nod (are atributul "nod.info")
-			"self" este obiect de tip NodParcurgere (are "self.nod_graf.info")
-		"""
-		pass # TO DO...		
+			"self" este obiect de tip NodParcurgere (are "self.nod_graf.info")"""
 
+		nod_c = self
+		while nod_c is not None:
+			if nod_c.nod_graf.info == nod.info:
+				return True
+			nod_c = nod_c.parinte
+		return False
 
-	#se modifica in functie de problema
+	# se modifica in functie de problema
 	def expandeaza(self):
 		"""Pentru nodul curent (self) parinte, trebuie sa gasiti toti succesorii (fiii)
 		si sa returnati o lista de tupluri (nod_fiu, cost_muchie_tata_fiu),
 		sau lista vida, daca nu exista niciunul.
 		(Fiecare tuplu contine un obiect de tip Nod si un numar.)
 		"""
-		pass # TO DO...
+		lista = []
+		for arc in self.problema.arce:
+			if arc.capat == self.nod_graf.info:
+				lista = lista + [[self.problema.cauta_nod_nume(arc.varf),arc.cost]]
+		return lista
 
-
-	#se modifica in functie de problema
+	# se modifica in functie de problema
 	def test_scop(self):
 		return self.nod_graf.info == self.problema.nod_scop
-
 
 	def __str__ (self):
 		parinte = self.parinte if self.parinte is None else self.parinte.nod_graf.info
 		return f"({self.nod_graf}, parinte={parinte}, f={self.f}, g={self.g})"
 
 
-
-""" Algoritmul A* """
-
-
-def str_info_noduri(l):
+# Algoritmul A*
+def str_info_noduri(lista):
 	"""
 		o functie folosita strict in afisari - poate fi modificata in functie de problema
 	"""
-	sir="["
-	for x in l:
-		sir+=str(x)+"  "
-	sir+="]"
+	sir = "["
+	for x in lista:
+		sir += str(x)+"  "
+	sir += "]"
 	return sir
 
 
-def afis_succesori_cost(l):
+def afis_succesori_cost(lista):
 	"""
 		o functie folosita strict in afisari - poate fi modificata in functie de problema
 	"""
-	sir=""
-	for (x, cost) in l:
-		sir+="\nnod: "+str(x)+", cost arc:"+ str(cost)
+	sir = ""
+	for (x, cost) in lista:
+		sir += "\nnod: " + str(x) + ", cost arc:" + str(cost)
 	
 	return sir
 
@@ -168,25 +156,46 @@ def in_lista(l, nod):
 
 def a_star():
 	rad_arbore = NodParcurgere(NodParcurgere.problema.nod_start)
-	open = [rad_arbore]		# open va contine elemente de tip NodParcurgere
-	closed = []				# closed va contine elemente de tip NodParcurgere
+	lopen = [rad_arbore]		# open va contine elemente de tip NodParcurgere
+	lclosed = []				# closed va contine elemente de tip NodParcurgere
+	nod_curent = None
+	while len(lopen) != 0:
+		nod_curent = lopen[0]
+		lclosed.append(nod_curent)
+		if nod_curent.nod_graf.info == rad_arbore.problema.nod_scop:
+			break	  # am gasit cel mai bun drum
+		lopen.remove(nod_curent)
+		succesori = nod_curent.expandeaza()
+		for (s,val) in succesori:
+			nod_nou = None
+			# daca apartine drumului sarim peste
+			if nod_curent.contine_in_drum(s):
+				continue
+			# nu apartine drumului
+			# verificam daca apare in lopen
+			aux = None
+			for n in lopen:
+				if n.nod_graf.info == s.info:
+					aux = n
+			if aux is not None and (aux.f > (nod_curent.g + val + s.h)):
+				lopen.remove(aux)
+			aux = None
+			for n in lclosed:
+				if n.nod_graf.info == s.info:
+					aux = n
+			if aux is not None and (aux.f > (nod_curent.g + val + s.h)):
+				lclosed.remove(aux)
+			nod_nou = NodParcurgere(s, nod_curent, nod_curent.g + val, nod_curent.g + val + s.h)
+			lopen.append(nod_nou)
+		lopen.sort(key=lambda x: (x.f,-x.g))
 
-	pass # TO DO...	
-	
-	
-	
-	
-		
 	print("\n------------------ Concluzie -----------------------")
-	if(len(open)==0):
+	if(len(lopen)==0):
 		print("Lista open e vida, nu avem drum de la nodul start la nodul scop")
 	else:
 		print("Drum de cost minim: " + str_info_noduri(nod_curent.drum_arbore()))
 	
 
-	
-	
-	
 if __name__ == "__main__":
 	problema = Problema()
 	NodParcurgere.problema = problema
